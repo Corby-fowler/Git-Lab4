d3f1554682603b75a4a7c8dcbc5a8a5e26d0da77
Corby-fowler <corby.b.fowler@student.uts.edu.au>
2021-08-29 15:16:24 +1000

    100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,12 +6,13 @@ public class PrintAndHide : MonoBehaviour
 {
     public int i;
     public Renderer rend;
-
+    int rand;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        rand = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -19,5 +20,16 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+
+        
+        if((gameObject.tag == "Red") && (i== 100)){
+            gameObject.SetActive(false);
+        }
+
+        if((gameObject.tag == "Blue") && i == rand)
+        {
+            rend.enabled = false;
+        }
+
     }
 }
